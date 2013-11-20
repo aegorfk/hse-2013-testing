e87b39473ffc8272721edb3ab8e016029120b5e0
Smouker1934@gmail.com <Smouker1934@gmail.com@41602369-7886-0db7-b472-67b8fcfdf3b4>
2013-11-20 18:33:37 +0000

    TestData.cs Added

---
+++ trunk/Testing/TestData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Testing
+{
+    class TestData
+    {
+        public static string SwitchPicture(Question Q)
+        {
+            string path = LoadTestFromFile.Path + "\\" + Q.Pict + ".jpg";
+            return path;
+        }
+
+        public static int Glav()
+        {
+            int glav = 0;
+            foreach (Question Q in LoadTestFromFile.Questions)
+            {
+                if (Q.Glava > glav) glav = Q.Glava;
+            }
+            return glav;
+        }
+
+        public static string Result()
+        {
+            int verno = 0;
+            for (int j = 0; j < Test.VoprosiTesta.Count(); j++)
+            {
+                if (Test.VoprosiTesta[j].Rightanswer == Test.ChosenAnswers[j]) verno++;
+            }
+            double percent = (((double) verno / (double) Test.VoprosiTesta.Count()) * 100.0);
+            string str = Math.Round(percent, 0).ToString();
+            return str;
+        }
+    }
+}
