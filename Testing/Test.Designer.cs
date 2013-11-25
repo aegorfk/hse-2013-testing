f48b925bb4cad3453056462e6a7f7a031abbc8dd
Smouker1934@gmail.com <Smouker1934@gmail.com@41602369-7886-0db7-b472-67b8fcfdf3b4>
2013-11-25 13:01:11 +0000

    Visual modifies

--- Testing/Test.Designer.cs
+++ Testing/Test.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Test));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.label1 = new System.Windows.Forms.Label();
@@ -58,6 +59,7 @@
             // 
             // label1
             // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(24, 321);
             this.label1.Name = "label1";
@@ -67,64 +69,69 @@
             // 
             // radioButton1
             // 
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
             this.radioButton1.Location = new System.Drawing.Point(27, 382);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(480, 40);
             this.radioButton1.TabIndex = 3;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.UseVisualStyleBackColor = false;
             // 
             // radioButton2
             // 
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
             this.radioButton2.Location = new System.Drawing.Point(27, 428);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(480, 40);
             this.radioButton2.TabIndex = 4;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.UseVisualStyleBackColor = false;
             // 
             // radioButton3
             // 
+            this.radioButton3.BackColor = System.Drawing.Color.Transparent;
             this.radioButton3.Location = new System.Drawing.Point(27, 474);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(480, 40);
             this.radioButton3.TabIndex = 5;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "radioButton3";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.UseVisualStyleBackColor = false;
             this.radioButton3.Visible = false;
             // 
             // radioButton4
             // 
+            this.radioButton4.BackColor = System.Drawing.Color.Transparent;
             this.radioButton4.Location = new System.Drawing.Point(27, 520);
             this.radioButton4.Name = "radioButton4";
             this.radioButton4.Size = new System.Drawing.Size(480, 40);
             this.radioButton4.TabIndex = 6;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "radioButton4";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.UseVisualStyleBackColor = false;
             this.radioButton4.Visible = false;
             // 
             // radioButton5
             // 
+            this.radioButton5.BackColor = System.Drawing.Color.Transparent;
             this.radioButton5.Location = new System.Drawing.Point(27, 566);
             this.radioButton5.Name = "radioButton5";
             this.radioButton5.Size = new System.Drawing.Size(480, 40);
             this.radioButton5.TabIndex = 7;
             this.radioButton5.TabStop = true;
             this.radioButton5.Text = "radioButton5";
-            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.UseVisualStyleBackColor = false;
             this.radioButton5.Visible = false;
             // 
             // nx
             // 
             this.nx.Location = new System.Drawing.Point(27, 621);
             this.nx.Name = "nx";
-            this.nx.Size = new System.Drawing.Size(75, 23);
+            this.nx.Size = new System.Drawing.Size(132, 25);
             this.nx.TabIndex = 8;
-            this.nx.Text = "nx";
+            this.nx.Text = "Следующий вопрос";
             this.nx.UseVisualStyleBackColor = true;
             this.nx.Click += new System.EventHandler(this.nx_Click);
             // 
@@ -132,9 +139,9 @@
             // 
             this.ps.Location = new System.Drawing.Point(206, 621);
             this.ps.Name = "ps";
-            this.ps.Size = new System.Drawing.Size(75, 23);
+            this.ps.Size = new System.Drawing.Size(133, 25);
             this.ps.TabIndex = 9;
-            this.ps.Text = "ps";
+            this.ps.Text = "Пропустить вопрос";
             this.ps.UseVisualStyleBackColor = true;
             this.ps.Click += new System.EventHandler(this.ps_Click);
             // 
@@ -142,9 +149,9 @@
             // 
             this.ex.Location = new System.Drawing.Point(449, 621);
             this.ex.Name = "ex";
-            this.ex.Size = new System.Drawing.Size(75, 23);
+            this.ex.Size = new System.Drawing.Size(75, 25);
             this.ex.TabIndex = 10;
-            this.ex.Text = "ex";
+            this.ex.Text = "Выход";
             this.ex.UseVisualStyleBackColor = true;
             this.ex.Click += new System.EventHandler(this.ex_Click);
             // 
@@ -152,6 +159,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Testing.Properties.Resources.фон;
             this.ClientSize = new System.Drawing.Size(536, 656);
             this.Controls.Add(this.ex);
             this.Controls.Add(this.ps);
@@ -163,8 +171,9 @@
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Test";
-            this.Text = "Testing";
+            this.Text = "Тестирование";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
